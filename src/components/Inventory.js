2766c4a736e69e9ead861daac508561f6a00e3cd
Islam Dudaev <islamwolf23@gmail.com>
2018-05-13 19:59:57 +0100

    Create an addFish form in Inventory

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,5 +1,11 @@
 import React from "react";
+import AddFishForm from "./AddFishForm";
 
-const Inventory = () => <div className="inventory">Inventory!!!</div>;
+const Inventory = () => (
+  <div className="inventory">
+    <h2>Inventory</h2>
+    <AddFishForm />
+  </div>
+);
 
 export default Inventory;
