8e5f8b050e0f6e0135317b35a057670f26b24d2e
Islam Dudaev <islamwolf23@gmail.com>
2018-05-13 13:40:42 +0100

    Get the router actually work

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,27 @@
 import React from "react";
-import {getFunName} from "../helpers";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  
+  myInput = React.createRef();
+
+  goToStore = e => {
+    e.preventDefault();
+    const storeName = this.myInput.value.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
-        <input type="text" placeholder="Store Name" defaultValue={getFunName()} required />
+        <input
+          type="text"
+          ref={this.myInput}
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          required
+        />
         <button type="submit">Visit Store</button>
       </form>
     );
