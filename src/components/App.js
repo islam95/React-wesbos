ac8a9c148ee9de2001e24d2ec8012fd6a949b485
Islam Dudaev <islamwolf23@gmail.com>
2018-05-16 11:13:09 +0100

    Create addToOrder function

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
-import Fish from './Fish';
+import Fish from "./Fish";
 
 class App extends React.Component {
   state = {
@@ -20,7 +20,16 @@ class App extends React.Component {
   loadSampleFishes = () => {
     this.setState({
       fishes: sampleFishes
-    })
+    });
+  };
+
+  addToOrder = key => {
+    // 1. take a copy of state
+    const order = { ...this.state.order };
+    // 2. Either add to the order, or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    // 3. Call setState to update our state object
+    this.setState({ order });
   };
 
   render() {
@@ -29,7 +38,13 @@ class App extends React.Component {
         <div className="menu">
           <Header tagline="Freash Seafood Market" />
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
           </ul>
         </div>
         <Order />
