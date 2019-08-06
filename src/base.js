b9f8ae90809fcb4efe225eb6b7d629d63fd7e614
Islam Dudaev <islamwolf23@gmail.com>
2019-08-06 23:18:35 +0100

    Firebase would be encrypted when published

--- src/base.js
+++ src/base.js
@@ -1,6 +1,7 @@
 import Rebase from "re-base";
 import firebase from "firebase";
 
+// This private information would be encrypted in real world app
 const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyBL9kar5ICxsGf-jXxGfAKfh-sQB6xUa48",
   authDomain: "react-wesbos-islam.firebaseapp.com",
